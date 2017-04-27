7a8827624c60fa0249ccf9eea006eb88cec2ae8e
Andres Lopez Urena <b43885@ecci.ucr.ac.cr>
2017-04-26 19:02:42 -0600

    feat: class definition of server

---
+++ server.py
@@ -0,0 +1,11 @@
+import socket
+import signal
+import time
+
+class Server:
+    def __init__(self, port = 8080):
+        self_host: ''
+        self_port = port
+
+    def initiateServer(self):
+        
